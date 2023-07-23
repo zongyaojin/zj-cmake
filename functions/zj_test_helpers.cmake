dac7077bc917318cf7434a0541f635fd5dd846a3
Zongyao Jin <zongyaojin@outlook.com>
2023-07-23 10:06:49 -0700

    moves test to a function

---
+++ functions/zj_test_helpers.cmake
@@ -0,0 +1,32 @@
+include_guard(GLOBAL)
+
+include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_variable_helpers.cmake)
+
+# ######################################################################################################################
+
+function(zj_test_setup filesAndFolders)
+
+    include(CTest)
+    find_package(GTest REQUIRED)
+    
+    zj_variable_prune(filesAndFolders)
+    foreach(zjTest IN LISTS zjAllTests)
+        set(testFile ${CMAKE_SOURCE_DIR}/tests/${zjTest}.cpp)
+        set(testFolder ${CMAKE_SOURCE_DIR}/tests/${zjTest})
+
+        if(EXISTS ${testFile})
+            add_executable(${zjTest} ${testFile})
+
+            # Since the combined library is created by the install function, it can be used to link all libraries
+            target_link_libraries(${zjTest} PUBLIC ${PROJECT_NAME}::${PROJECT_NAME} GTest::GTest GTest::Main)
+            set_target_properties(${zjTest} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
+            add_test(NAME ${zjTest} COMMAND ${CMAKE_BINARY_DIR}/tests/${zjTest})
+        endif()
+
+        if(IS_DIRECTORY ${testFolder})
+            add_subdirectory(${testFolder})
+        endif()
+
+    endforeach()
+
+endfunction()
--- zj-include-all.cmake
+++ zj-include-all.cmake
@@ -12,6 +12,7 @@ include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_path_helpers.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_target_helpers.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_system_helpers.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_git_helpers.cmake)
+include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_test_helpers.cmake)
 
 include(${CMAKE_SOURCE_DIR}/cmake/installation/zj_install_helpers.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/utilities/CCache.cmake)
