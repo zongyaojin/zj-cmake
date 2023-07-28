2beff85d32047a99d86f48cca9f5f0872f8540ae
Zongyao Jin <zongyaojin@outlook.com>
2023-07-27 23:51:37 -0700

    adds comments to test function

--- functions/zj_test_helpers.cmake
+++ functions/zj_test_helpers.cmake
@@ -19,7 +19,9 @@ function(zj_test_setup filesAndFolders)
 
             # Since the combined library is created by the install function, it can be used to link all libraries
             target_link_libraries(${zjTest} PUBLIC ${PROJECT_NAME}::${PROJECT_NAME} GTest::GTest GTest::Main)
+            # Set runtime output directory
             set_target_properties(${zjTest} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
+            # Specify the command to execute for running the test
             add_test(NAME ${zjTest} COMMAND ${CMAKE_BINARY_DIR}/tests/${zjTest})
         endif()
 
