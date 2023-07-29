420cf52d835cb78010b14f6f713bc7735a3c3a87
Zongyao Jin <zongyaojin@outlook.com>
2023-07-29 08:16:08 -0700

    adds install-dep script

---
+++ scripts/zj-install-deps.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Google style guide: https://google.github.io/styleguide/shellguide.html#s7.4-source-filenames
+
+# Exit the script on any error
+set -e
+
+# Script absolute path
+script_abs_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Install system-level package managment dependencies
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y build-essential pkg-config git wget autoconf libtool shfmt
+sudo apt install -y cmake
+
+# Install python dependencies
+pip install --user cmakelang
