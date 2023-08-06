14efbddd2fce8ac48d8b7e2d70b66e760c97f654
Zongyao Jin <zongyaojin@outlook.com>
2023-08-05 21:29:32 -0700

    adds compile definition paths to have no trailing slash

--- definitions/ZJ-compile-definitions.cmake
+++ definitions/ZJ-compile-definitions.cmake
@@ -15,7 +15,7 @@ add_compile_definitions(__ZJ_PKG_BUILD_PATH_NO_SLASH__="${CMAKE_BINARY_DIR}")
 # Variable CMAKE_INSTALL_BINDIR is not a built-in variable in CMake, but it is defined in the GNUInstallDirs module,
 # which provides many variables you can use to reference common destination directories in a platform-independent way.
 include(GNUInstallDirs)
-add_compile_definitions(__ZJ_PKG_INSTALL_BIN_PATH__="${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}/")
+add_compile_definitions(__ZJ_PKG_INSTALL_BIN_PATH_NO_SLASH__="${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}")
 
 # Git and build info
 zj_git_get_info(
