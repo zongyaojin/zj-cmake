0dfc159ea91f2f3f3c92b8b1e9dd839a54edd598
Zongyao Jin <zongyaojin@outlook.com>
2023-08-07 23:29:54 -0700

    adds code coverage support

---
+++ definitions/ZJ-code-coverage.cmake
@@ -0,0 +1,14 @@
+include_guard(GLOBAL)
+
+# ######################################################################################################################
+
+if(CMAKE_COMPILER_IS_GNUCXX AND ZJ_CODE_COVERAGE)
+    # * https://discourse.cmake.org/t/guideline-for-code-coverage/167
+    add_compile_options(-fprofile-arcs -ftest-coverage)
+
+    # * See https://stackoverflow.com/a/6090147
+    # * And https://cmake.org/cmake/help/latest/variable/CMAKE_EXE_LINKER_FLAGS.html
+    # * Without this flag, all executables will have to be linked to library "gcov" manually; otherwise, with the above
+    #   compiler options, they will look for something in the "gcov" library which they cannot find if not linked
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-coverage")
+endif()
--- zj-include-all.cmake
+++ zj-include-all.cmake
@@ -6,6 +6,7 @@ include(${CMAKE_SOURCE_DIR}/cmake/definitions/ZJ-colors.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/definitions/ZJ-compiler-options.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/definitions/ZJ-compile-definitions.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/definitions/ZJ-find-settings.cmake)
+include(${CMAKE_SOURCE_DIR}/cmake/definitions/ZJ-code-coverage.cmake)
 
 include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_file_helpers.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/functions/zj_path_helpers.cmake)
