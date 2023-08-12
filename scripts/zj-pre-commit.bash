89e80543320fc8fcebc6aead3e20db5c567afb6e
Zongyao Jin <zongyaojin@outlook.com>
2023-08-12 00:59:09 -0700

    up

--- scripts/zj-pre-commit.bash
+++ scripts/zj-pre-commit.bash
@@ -9,10 +9,10 @@ script_abs_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # Path definitions
 source_path=$script_abs_path/..
 
-# Go to the build directory, build and install
+# Go to the source path, set up and run pre-commit
 (
     cd $source_path
-    pre-commit autoupdate
     pre-commit install
+    pre-commit autoupdate
     pre-commit run -a
 )
