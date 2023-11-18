81908f2e633ba61b71f80f51d527a8da5427e222
Zongyao Jin <zongyaojin@outlook.com>
2023-11-17 17:10:42 -0800

    sets up pre-commit

--- scripts/zj-pre-commit.bash
+++ scripts/zj-pre-commit.bash
@@ -1,17 +1,25 @@
 #!/bin/bash
+set -exo pipefail
 
-# https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
-set -euxo pipefail
+script_file=$(realpath "$0")
+script_absolute_path=$(dirname "$script_file")
+package_path=$(realpath $script_absolute_path/../..)
 
-# Script absolute path
-script_abs_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+sudo apt update -y
+sudo apt upgrade -y
 
-# Path definitions
-source_path=$script_abs_path/..
+# Install pip
+sudo apt install -y python3-pip
 
-# Go to the source path, set up and run pre-commit
+# Upgrade pip and setuptools (included in Python installation)
+pip install --upgrade pip setuptools
+
+# Install cmakelang and pre-commit
+pip install --user cmakelang pre-commit
+
+# Go to the package path, set up and run pre-commit
 (
-    cd $source_path
+    cd $package_path
     pre-commit install
     pre-commit autoupdate
     pre-commit run -a
